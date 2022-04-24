27ea9e539a4968cc7db70cc31459e1d01a7bd0ed
TammamWafai <47179662+TammamWafai@users.noreply.github.com>
2022-04-24 18:58:08 -0500

    Nav fetched build

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-alert("hiiiiiii");
+const SCRIPT = {
+    urls: {
+        base: 'https://swapi.dev/api/',
+        people: 'people/',
+        planets: 'planets/',
+        films: 'films/',
+        species: 'species/',
+        vehicles: 'vehicles/',
+        starships: 'starships/',
+    },
+    init: () => {
+        SCRIPT.addListeners();
+        SCRIPT.buildNav();
+    },
+    addListeners: () => {
+        let nav = document.getElementById('nav');
+        nav.addEventListener('click', SCRIPT.getData);
+        footer.addEventListener('click', SCRIPT.getData);
+    },
+    buildNav: () => {
+        let df = new DocumentFragment();
+        for (let nm in SCRIPT.urls) {
+            if (nm != 'base') {
+                let link = document.createElement('a');
+                link.href = `${SCRIPT.urls.base}${SCRIPT.urls[nm]}`;
+                link.textContent = nm;
+                link.setAttribute('data-link', `${SCRIPT.urls.base}${SCRIPT.urls[nm]}`);
+                df.append(link);
+            }
+        }
+        document.getElementById('nav').append(df);
+    },
+
+    getData: (ev) => {
+        if (ev) ev.preventDefault();
+
+    },
+    buildList: (data) => {
+        let m = document.getElementById('main');
+        console.log(data);
+
+        let footer = document.getElementById('footer');
+        footer.innerHTML = '';
+
+        if (data.previous) {
+
+        }
+        if (data.next) {
+
+        }
+    },
+};
+
+document.addEventListener('DOMContentLoaded', SCRIPT.init);
